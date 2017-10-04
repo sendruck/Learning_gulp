829f2a305a754695e990069904e37db2588677af
OlexandrYakovenko <olexandr.yakov@gmail.com>
2017-10-04 22:03:05 +0300

    Added sass compilation, and changed in dut to recompiled on changes

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
- var gulp = require('gulp');
-     uglify = require('gulp-uglify');
+ var gulp = require('gulp'),
+     uglify = require('gulp-uglify'),
+     sass = require('gulp-ruby-sass');
  
 // Scripts Task 
 //Uglifies, 
@@ -10,10 +11,19 @@ gulp.task('scripts', function() {
 });
 
 
+//Styles Task 
+gulp.task('styles', function() {
+    return sass('scss/*.scss', {
+     style: 'compressed'
+    })
+     .pipe(gulp.dest('css'));
+   });
+
 //Watch task
 //Watch JS
 gulp.task('watch', function() {
     gulp.watch('*.js', ['scripts']);
+    gulp.watch('scss/*.scss', ['styles']);
 }); 
     
- gulp.task('default', ['scripts', 'watch']); 
+ gulp.task('default', ['scripts', 'watch']);  
