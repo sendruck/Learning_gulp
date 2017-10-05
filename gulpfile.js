21325347b135ea5bc79bf163c5593f4b383028ab
OlexandrYakovenko <olexandr.yakov@gmail.com>
2017-10-05 21:12:41 +0300

    Added plumber plugin to faciliate debug gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,24 @@
  var gulp = require('gulp'),
      uglify = require('gulp-uglify'),
-     sass = require('gulp-ruby-sass');
+     sass = require('gulp-ruby-sass'),
+     plumber = require('gulp-plumber');
  
 // Scripts Task 
 //Uglifies, 
 gulp.task('scripts', function() {
     gulp.src('*.js')
-    .pipe(uglify())
-    .pipe(gulp.dest('build/js'));  
+        .pipe(plumber())
+        .pipe(uglify())
+        .pipe(gulp.dest('build/js'));  
 });
 
 
 //Styles Task 
-gulp.task('styles', function() {
+gulp.task('styles', function() {   
     return sass('scss/*.scss', {
      style: 'compressed'
-    })
+    })    
+     .pipe(plumber())    
      .pipe(gulp.dest('css'));
    });
 
